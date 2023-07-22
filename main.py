c9f6b5694ff5b1c2df5a2bf746ce844aeeac53fb
ulugbekpy <ulugbekpycpp@gmail.com>
2023-07-22 21:11:06 +0500

    makefile for making commands easier

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello":"World"}
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("items/create/")
+def create_item(item_id: int, item_name: str, item_price: float):
+    return {"item_name": item_name, "item_price": item_price}
 
 
 @app.put("/items/{item_id}")
